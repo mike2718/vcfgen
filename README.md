92dcc60c951f281a9e8daa01ab880b90663e8a35
Mike Akiba <mike2718@gmail.com>
2021-12-02 09:52:10 +0800

    v0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# vCard(.vcf)生成器
+# vCard生成器
+
+## 简介
+
+因为使用过很多vcf生成器，都很烂，于是想办法自己做一个。
+
+## 用法
+
+```
+$ clang -o vcfgen vcfgen.c && ./vcfgen
+```
+
+默认生成的文件名为`my_card.vcf`
+
---
+++ vcfgen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    char xm[100];
+    char sj[100];
+    char yx[100];
+    FILE *fp = NULL;
+
+    printf("vCard生成器 ver.0.1.0\n");
+    printf( "请输入姓名：");
+    scanf("%s", xm);
+    printf( "请输入手机：");
+    scanf("%s", sj);
+    printf( "请输入邮箱地址：");
+    scanf("%s", yx);
+
+    fp = fopen("./my_card.vcf", "w");
+    fprintf(fp, "BEGIN:VCARD\r\n");
+    fprintf(fp, "VERSION:4.0\r\n");
+    fprintf(fp, "FN:%s\r\n", xm);
+    fprintf(fp, "EMAIL;type=INTERNET;type=WORK;type=pref:%s\r\n", yx);
+    fprintf(fp, "TEL;type=CELL:%s\r\n", sj);
+    fprintf(fp, "END:VCARD\r\n");
+    fprintf(fp, "\r\n");
+    //fprintf(fp, "This is testing for fprintf...\n");
+    //fputs("This is testing for fputs...\n", fp);
+    fclose(fp);
+    return 0;
+}
