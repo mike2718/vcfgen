ba7db9aeaf2abbe73e067a062aae9618550847bb
Mike Akiba <mike2718@gmail.com>
2024-08-27 00:23:06 +0800

    使得其符合v3.0的VCard规范

--- vcfgen.c
+++ vcfgen.c
@@ -2,7 +2,7 @@
 //作用：
 //  在当前目录下，生成名为my_card.vcf的vCard v.4.0文件
 //用法：
-//  % clang -Wall -Wpedantic -Wextra -Werror -g -O0 -std=c99 -o vcfgen vcfgen.c
+//  % gcc -Wall -Wpedantic -Wextra -Werror -g -O0 -std=c99 -o vcfgen vcfgen.c
 //  % ./vcfgen
 #include <stdio.h>
 int main(void)
@@ -13,7 +13,7 @@ int main(void)
     //char wz[2000];
     FILE *fp = NULL;
 
-    printf("vCard生成器 ver.0.1.0\n");
+    printf("vCard生成器 ver.0.1.1\n");
     printf( "请输入姓名：");
     scanf("%s", xm);
     printf( "请输入手机：");
@@ -25,10 +25,10 @@ int main(void)
 
     fp = fopen("./my_card.vcf", "w");
     fprintf(fp, "BEGIN:VCARD\r\n");
-    fprintf(fp, "VERSION:4.0\r\n");
+    fprintf(fp, "VERSION:3.0\r\n");
     fprintf(fp, "FN:%s\r\n", xm);
-    fprintf(fp, "TEL;VALUE=uri;TYPE=\"cell,voice\":tel:%s\r\n", sj);
-    fprintf(fp, "EMAIL;TYPE=:%s\r\n", yx);
+    fprintf(fp, "TEL;TYPE=cell,voice:%s\r\n", sj);
+    fprintf(fp, "EMAIL;TYPE=internet:%s\r\n", yx);
     //fprintf(fp, "URL:%s\r\n", wz);
     fprintf(fp, "END:VCARD\r\n");
     fclose(fp);
